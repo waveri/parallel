c27ac6f2a0b3957e1881469eb065d646875c63ec
waveri <pratik05cse@gmail.com>
2024-09-16 01:58:17 +0530

    all good

---
+++ Bubble.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+#include <stdio.h>
+
+void bubble_sort(int arr[], int n) {
+    int i, j, temp, swapped;
+    for (i = 0; i < n - 1; i++) {
+        swapped = 0;
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = 1;
+            }
+        }
+        if (swapped == 0) {
+            break;
+        }
+    }
+}
+
+void print_array(int arr[], int n) {
+    int i;
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {5, 2, 8, 1, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printf("Original array: ");
+    print_array(arr, n);
+    bubble_sort(arr, n);
+    printf("Sorted array: ");
+    print_array(arr, n);
+    return 0;
+}
